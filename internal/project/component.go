627346bebb604e0a8a594dd8e722132c16d73e35
James <pavelnedved010@hotmail.com>
2026-07-19 13:42:47 -0300

    feat(project): strengthen domain model

--- internal/project/component.go
+++ internal/project/component.go
@@ -1,7 +1,16 @@
 package project
 
+type ComponentType string
+
+const (
+	ComponentSkill    ComponentType = "skill"
+	ComponentMemory   ComponentType = "memory"
+	ComponentWorkflow ComponentType = "workflow"
+	ComponentTemplate ComponentType = "template"
+)
+
 type Component struct {
 	Name string
-	Type string
+	Type ComponentType
 	Path string
 }
